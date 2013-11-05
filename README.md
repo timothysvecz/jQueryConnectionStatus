0619cef8f567d274315b731c3944bd7ac37339b3
Timothy Svecz <social@timsve.cz>
2013-11-05 17:27:56 -0500

    change options to object literal

--- README.md
+++ README.md
@@ -11,6 +11,34 @@ To use: Include the ConnectionStatus.js file in your page as appropriate.
 				connectioncheck();
 			});
 			
+			
+			
+			
+If you want to customize the implmentation, set the options:
+
+			$(document).ready(function() {
+				connectioncheck({ remoteaddress: "http://www.google.com", frequency: 3 });
+			});
+
+
+
+Available options:
+
+frequency: Frequency to check connection in seconds
+
+remotepole: Change to true to send the AJAX request to the address you specify. 
+			This is helpful if you dont want to make the request to the clients current page.
+			
+remoteaddress: Only needed if remotepole is true. This will be the address the plugin will make the xhr request to.
+
+errormessage: This is the message the client will see if using the native DIV
+
+usenativediv: False will call show()/hide() on a DIV you specify by id. This is a usefull way to use this plugin with your sites existing features.
+			
+foreigndivid: Only needed if you dont want to use my div. Make sure you hide it in document ready or it will be there for a few seconds and confuse the client.
+
+inputstodisable: An array of input types to disable when connectivity is not available. See http://www.w3schools.com/tags/att_input_type.asp for available types.
+
 =======================
 To congifure: Open the ConnectionStatus.js file. Settings are available at the top.
 
@@ -19,3 +47,4 @@ To congifure: Open the ConnectionStatus.js file. Settings are available at the t
 Requirements: jQuery
 
 =======================
+
--- js/ConnectionStatus.js
+++ js/ConnectionStatus.js
@@ -3,50 +3,39 @@
  *
  *
  * Use under MIT-License
- * This is the only file neededed. Simply include the file and change the options below
+ * This is the only file neededed.
  */
-
-/**
- * OPTIONS/SETTINGS
- */
-var frequency = 10;
-//Frequency to check connection in seconds
-var remotepole = true;
-// Change to true to send the AJAX request to the address you specify. This is helpful if you dont want to make the request to the clients current page.
-var remoteaddress = "http://www.yahoo.com";
-// Only needed if remotepole is true
-var errormessage = "Connection to the server has been interupted. Please Try Again.";
-// This is the message the user will see.
-var usenativediv = true;
-//Falsewill call show()/hide() on a div you specify by id. This is a usefel way to use this plugin with your sites existing features.
-var foreigndivid = "myDIVid";
-//Only needed if you dont want to use my div. Make sure you hide it in document ready or it will be there for a few seconds and confuse the client.
-var disableinputsubmit = true;
-//This will disable input buttons of type submit when connections are not available.
-var disableinputbutton = true;
-//This will disable input buttons of type button when connections are not available.
-var disableinputtext = true;
-//This will disable input of type text when connections are not available.
-//TIP: you can use any type http://www.w3schools.com/tags/att_input_type.asp   $('input[type=submit]').attr('disabled', true);
-
 /**
  * The code below uses setinterval to send an ajax request at specified intervals and then do stuff based on the status
  */
-
 var currentstatus = 0;
-//So we can keep track of the prior connection status
-
+var frequency = 10;
+var remotepole = false;
+var remoteaddress = "http://www.yahoo.com";
+var errormessage = "Connection to the server has been interupted. Please Try Again.";
+var usenativediv = true;
+var foreigndivid = "myDIVid";
+var inputstodisable = "";
 var connectionaddress = "";
-function connectioncheck() {
+function connectioncheck(options) {
+	var options = options || {};
+	frequency = options.frequency || 10;
+	remotepole = options.remotepole || false;
+	remoteaddress = options.remoteaddress || "http://www.yahoo.com";
+	errormessage = options.errormessage || "Connection to the server has been interupted. Please Try Again.";
+	usenativediv = options.usenativediv || true;
+	foreigndivid = options.foreigndivid || "myDIVid";
+	inputstodisable = options.inputstodisable;
+	if (usenativediv == false) {
+		$('#' + foreigndivid).hide();
+	}
 	if (!remotepole) {
 		connectionaddress = document.URL;
 	} else {
 		connectionaddress = remoteaddress;
 	}
-	//check how the user wants to check the status
 	setInterval(function() {
 		var activeRequest = false;
-		//So there are not duplicate requests
 		if (!activeRequest) {
 			activeRequest = true;
 			var request = $.ajax({
@@ -68,43 +57,30 @@ function connectioncheck() {
 };
 function handleStatus(status) {
 	if (status === 1) {
-
-		if (currentstatus === 0) {
-			if (disableinputsubmit == true) {
-				$('input[type=submit]').attr('disabled', false);
-			}
-			if (disableinputbutton == true) {
-				$('input[type=button]').attr('disabled', false);
-			}
-			if (disableinputtext == true) {
-				$('input[type=text]').attr('disabled', false);
+		
+			if (inputstodisable != null && inputstodisable.length > 0) {
+				$.each(inputstodisable, function(index, value) {
+					$('input[type=' + value + ']').attr('disabled', false);
+				});
 			}
 			if (usenativediv == true) {
 				$('#alertdiv').remove();
 			} else {
-					$('#' + foreigndivid).hide();
+				$('#' + foreigndivid).hide();
 			}
-		}
-		//only mess with the DOM if the prior status was opposite this one
+		
 		currentstatus = 1;
 	} else {
-
-		if (currentstatus === 1) {
-			if (disableinputsubmit == true) {
-				$('input[type=submit]').attr('disabled', true);
-			}
-			if (disableinputbutton == true) {
-				$('input[type=button]').attr('disabled', true);
-			}
-			if (disableinputtext == true) {
-				$('input[type=text]').attr('disabled', true);
-			}
-			if (usenativediv == true) {
-				var thediv = "<div style='position:fixed;bottom:0;left:0px;text-align:center;font-size:25px;background-color:#EACBCB;color:#BF6566;width:100%;' id='alertdiv' >" + errormessage + "</div>";
-				$('body').append($(thediv));
-			} else {
-				$('#' + foreigndivid).show();
-			}
+		if ( inputstodisable != null && inputstodisable.length > 0) {
+			$.each(inputstodisable, function(index, value) {
+				$('input[type=' + value + ']').attr('disabled', true);
+			});
+		}
+		if (usenativediv == true) {
+			var thediv = "<div style='position:fixed;bottom:0;left:0px;text-align:center;font-size:25px;background-color:#EACBCB;color:#BF6566;width:100%;' id='alertdiv' >" + errormessage + "</div>";
+			$('body').append($(thediv));
+		} else {
+			$('#' + foreigndivid).show();
 		}
 		currentstatus = 0;
 	}
